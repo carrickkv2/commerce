ab297b31d76df56703ef54d251c1e52db84f9d6e
David Asmah <davidasmah@hotmail.com>
2021-10-21 10:27:37 +0000

    Added main files.

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import User, AuctionListings, Bids, Comments, WatchList
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'date_joined']
+    search_fields = ('username', 'email')
+    readonly_fields = ('date_joined',)
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(AuctionListings)
+admin.site.register(Bids)
+admin.site.register(Comments)
+admin.site.register(WatchList)
