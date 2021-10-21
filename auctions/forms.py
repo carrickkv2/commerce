a337d6c1b165051294745c8eb69092277bc203d4
David Asmah <davidasmah@hotmail.com>
2021-10-21 10:27:41 +0000

    Added main files.

---
+++ auctions/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'date_joined')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'date_joined')
