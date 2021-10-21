ac119adf0916201f18ba78057a429e48d91bbb0f
David Asmah <davidasmah@hotmail.com>
2021-10-21 17:34:42 +0000

    Updated settings.py.

--- commerce/settings.py
+++ commerce/settings.py
@@ -118,6 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
 AUTH_USER_MODEL = 'auctions.User'
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
