e720e3c0dcf0d4d8c3554f7c2d99fcd6c4db586e
David Asmah <davidasmah@hotmail.com>
2021-11-04 11:58:47 +0000

    Updated database

--- auctions/views.py
+++ auctions/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from .models import User, AuctionListings
 from django.forms import ModelForm
@@ -18,13 +18,16 @@ def index(request):
     """
     Returns the main page of the site.
     """
+    main_banner = get_object_or_404(AuctionListings, title="YEEZY BOOST 350")
+    featured1 = get_object_or_404(AuctionListings, title="Nike Jordan Max")
+    # featured2 = get_object_or_404(AuctionListings, title="YEEZY BOOST 350")
+    # featured3 = get_object_or_404(AuctionListings, title="YEEZY BOOST 350")
     return render(
         request,
         "auctions/index.html",
         {
-            "main_banner": AuctionListings.objects.get(
-                title="YEEZY BOOST 350"
-            ),  # Should get the lisiting for only the main banner
+            "main_banner": main_banner,
+            "featured1": featured1,
             "current_year": datetime.datetime.now().year,
         },
     )
@@ -104,9 +107,7 @@ def create(request):
         author.seller = current_user
         author.save()
 
-        return render(
-            request, "auctions/index.html"
-        )  # Should be the specific listing here
+        return redirect("listing_page", listing_id=author.pk)
 
     return render(request, "auctions/create_listings.html", {"form": NewListingForm()})
 
@@ -116,4 +117,18 @@ def all_listings(request):
     Shows all listings currently on the site.
     """
 
-    return render(request, "auctions/all_listings.html")
+    return render(
+        request, "auctions/all_listings.html"
+    )  # Loop through all listings here
+
+
+def listing_page(request, listing_id):
+    """
+    Shows the specifics of a listing.
+    """
+    listing = get_object_or_404(AuctionListings, pk=listing_id)
+    return render(
+        request,
+        "auctions/listing_page.html",
+        {"listing": listing},
+    )
